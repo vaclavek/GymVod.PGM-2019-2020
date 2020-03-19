aad989b5f7721c22800b2875bc48ceb43e0ae9e6
JanPro <janproxdd@gmail.com>
2020-03-19 21:46:43 +0100

    ukol

---
+++ 2. pololetí/Domácí úkoly/Provazník/todolist/Login.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TodoList
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
