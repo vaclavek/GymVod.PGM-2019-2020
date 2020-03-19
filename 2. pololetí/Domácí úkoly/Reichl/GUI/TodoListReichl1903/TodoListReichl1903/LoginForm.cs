7b9291521e973d396edc3985532790da90fb8766
Tomáš Reichl <krtkus001@gmail.com>
2020-03-19 17:35:42 +0100

    add domaci ukol 19.3. Reichl

---
+++ 2. pololetí/Domácí úkoly/Reichl/GUI/TodoListReichl1903/TodoListReichl1903/LoginForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoListReichl1903
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+            logins.Add("jmeno", new byte[] { 0x56, 0xB1, 0xDB, 0x81, 0x33, 0xD9, 0xEB, 0x39, 0x8A, 0xAB, 0xD3, 0x76, 0xF0, 0x7B, 0xF8, 0xAB, 0x5F, 0xC5, 0x84, 0xEA, 0x0B, 0x8B, 0xD6, 0xA1, 0x77, 0x02, 0x00, 0xCB, 0x61, 0x3C, 0xA0, 0x05 });
+        }
+
+        /* přihlašovací údaje:
+         * "jmeno"
+         * "heslo"
+         * heslo ve formě SHA256 hashe (aby to bylo trochu realističtější)
+         */
+        private Dictionary<string, byte[]> logins = new Dictionary<string, byte[]>();
+
+        private SHA256 sha = SHA256.Create();
+
+
+        private void butSignin_Click(object sender, EventArgs e)
+        {
+            
+            // debug kód
+            Console.WriteLine(tbUsername.Text);
+            Console.WriteLine(tbPassword.Text);
+            Console.WriteLine("0x"+BitConverter.ToString(sha.ComputeHash(new MemoryStream(Encoding.UTF8.GetBytes(tbPassword.Text)))).Replace("-", ",0x"));
+            Console.WriteLine();
+
+            if (logins.ContainsKey(tbUsername.Text))
+            {
+                if (logins[tbUsername.Text].SequenceEqual(sha.ComputeHash(new MemoryStream(Encoding.UTF8.GetBytes(tbPassword.Text)))))
+                {
+                    this.Hide();
+                    new MainForm().ShowDialog();
+                    this.Close();
+                }
+                else
+                    tsslSigninError.Text = "Špatné heslo";
+            }
+            else
+                tsslSigninError.Text = "Zadané uživatelské jméno neexistuje";
+        }
+    }
+}
---
+++ 2. pololetí/Domácí úkoly/Reichl/GUI/TodoListReichl1903/TodoListReichl1903/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TodoListReichl1903
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.tbNote = new System.Windows.Forms.TextBox();
+            this.butAdd = new System.Windows.Forms.Button();
+            this.butDel = new System.Windows.Forms.Button();
+            this.lbTodos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Úkol:";
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(50, 3);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(170, 20);
+            this.dtpDate.TabIndex = 1;
+            // 
+            // tbNote
+            // 
+            this.tbNote.Location = new System.Drawing.Point(226, 2);
+            this.tbNote.Name = "tbNote";
+            this.tbNote.Size = new System.Drawing.Size(487, 20);
+            this.tbNote.TabIndex = 2;
+            // 
+            // butAdd
+            // 
+            this.butAdd.Location = new System.Drawing.Point(719, 0);
+            this.butAdd.Name = "butAdd";
+            this.butAdd.Size = new System.Drawing.Size(75, 23);
+            this.butAdd.TabIndex = 3;
+            this.butAdd.Text = "Přidat";
+            this.butAdd.UseVisualStyleBackColor = true;
+            this.butAdd.Click += new System.EventHandler(this.butAdd_Click);
+            // 
+            // butDel
+            // 
+            this.butDel.Location = new System.Drawing.Point(719, 29);
+            this.butDel.Name = "butDel";
+            this.butDel.Size = new System.Drawing.Size(75, 23);
+            this.butDel.TabIndex = 4;
+            this.butDel.Text = "Odstranit";
+            this.butDel.UseVisualStyleBackColor = true;
+            this.butDel.Click += new System.EventHandler(this.butDel_Click);
+            // 
+            // lbTodos
+            // 
+            this.lbTodos.FormattingEnabled = true;
+            this.lbTodos.Location = new System.Drawing.Point(12, 29);
+            this.lbTodos.Name = "lbTodos";
+            this.lbTodos.Size = new System.Drawing.Size(701, 420);
+            this.lbTodos.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbTodos);
+            this.Controls.Add(this.butDel);
+            this.Controls.Add(this.butAdd);
+            this.Controls.Add(this.tbNote);
+            this.Controls.Add(this.dtpDate);
+            this.Controls.Add(this.label1);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox tbNote;
+        private System.Windows.Forms.Button butAdd;
+        private System.Windows.Forms.Button butDel;
+        private System.Windows.Forms.ListBox lbTodos;
+    }
+}
