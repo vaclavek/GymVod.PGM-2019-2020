257676a445b2032f8f5b1f1d65b6eb679dfc6922
JanPro <janproxdd@gmail.com>
2020-03-19 21:46:43 +0100

    ukol

---
+++ 2. pololetí/Domácí úkoly/Provazník/todolist/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TodoList
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
