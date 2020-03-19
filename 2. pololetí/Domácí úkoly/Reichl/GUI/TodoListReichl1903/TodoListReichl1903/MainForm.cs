09a26c31e672e4062bc6c7d1f8ad2389d918497b
Tomáš Reichl <krtkus001@gmail.com>
2020-03-19 17:35:42 +0100

    add domaci ukol 19.3. Reichl

---
+++ 2. pololetí/Domácí úkoly/Reichl/GUI/TodoListReichl1903/TodoListReichl1903/MainForm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoListReichl1903
+{
+    public partial class MainForm : Form
+    {
+        private BindingList<TodoItem> todoItems = new BindingList<TodoItem>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            lbTodos.ValueMember = nameof(TodoItem.Id);
+            lbTodos.DisplayMember = nameof(TodoItem.DisplayName);
+            lbTodos.DataSource = todoItems;
+        }
+
+        private void butAdd_Click(object sender, EventArgs e) => todoItems.Add(new TodoItem { Date = dtpDate.Value, Name = tbNote.Text });
+       
+
+        private void butDel_Click(object sender, EventArgs e) => todoItems.Remove((TodoItem)lbTodos.SelectedItem);
+        
+    }
+}
---
+++ 2. pololetí/Domácí úkoly/Reichl/GUI/TodoListReichl1903/TodoListReichl1903/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListReichl1903
+{
+    class TodoItem
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string DisplayName { get => $"{Date.ToShortDateString()} - {Name}"; }
+    }
+}
