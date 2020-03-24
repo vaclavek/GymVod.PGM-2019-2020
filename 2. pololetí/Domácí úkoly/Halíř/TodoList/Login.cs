6ee7c0c9ec761ef55e77c8ef17a6eebb4f0841b3
Ondřej Václavek <vaclavek@jinsoft.cz>
2020-03-24 21:04:34 +0100

    Domácí úkoly

---
+++ 2. pololetí/Domácí úkoly/Halíř/TodoList/Login.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TodoList
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (LoginValidity()== false)
+            {
+                MessageBox.Show("Either the username, or the password is wrong", caption:"Invalid Login");
+                return;
+            }
+            else
+            {
+                this.Hide();
+                MainForm MainForm = new MainForm();
+                MainForm.ShowDialog();
+                this.Close();
+            }
+
+        }
+        private bool LoginValidity()
+        {
+            string correctUsername = "GymVod";
+            string correctPassword = "666.666";
+
+            if(correctUsername==UsernameTB.Text && correctPassword==PasswordTB.Text)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 2. pololetí/Domácí úkoly/Halíř/TodoList/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TodoList
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            if (TextTB.Equals(null))
+            {
+                ListLB.Items.Add(DateDTP.Value.Date.ToString().Remove(10, 9) + " : " + TextTB.Text);
+                //Snaha se zbavit času v listu. Plánuji zaměnit za vlastní třídu s hodnotou DateTime pokud to půjde. ListBox se sám uspořádává
+            }
+            else
+            {
+                MessageBox.Show("No input in text box", "Invalid input");
+            }
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            ListLB.Items.RemoveAt(ListLB.SelectedIndex);
+        }
+
+        private void ListLB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            /*if(sender.Equals(Keys.Delete))
+            {
+                DeleteBtn_Click(sender,e);
+            }
+            Snažil jsem se rychle vytvořit možnost použít tlačítko delete pro odstranění objektu
+            */
+        }
+    }
+}
