9e013f7e87c9452ab6c4e35f8f883f18eadef207
JanFlajz <62598168+JanFlajz@users.noreply.github.com>
2020-03-25 21:45:16 +0100

    Add files via upload

---
+++ 2. pololetí/Domácí úkoly/Flajžík/TodolistUkol2/Login.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TodoList
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsernameTB = new System.Windows.Forms.TextBox();
+            this.PasswordTB = new System.Windows.Forms.TextBox();
+            this.LoginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Přihlašovací jméno:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Heslo:";
+            // 
+            // UsernameTB
+            // 
+            this.UsernameTB.Location = new System.Drawing.Point(119, 6);
+            this.UsernameTB.Name = "UsernameTB";
+            this.UsernameTB.Size = new System.Drawing.Size(155, 20);
+            this.UsernameTB.TabIndex = 2;
+            // 
+            // PasswordTB
+            // 
+            this.PasswordTB.Location = new System.Drawing.Point(119, 33);
+            this.PasswordTB.Name = "PasswordTB";
+            this.PasswordTB.PasswordChar = '*';
+            this.PasswordTB.Size = new System.Drawing.Size(155, 20);
+            this.PasswordTB.TabIndex = 3;
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.Location = new System.Drawing.Point(119, 59);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(155, 23);
+            this.LoginBtn.TabIndex = 4;
+            this.LoginBtn.Text = "Přihlásit";
+            this.LoginBtn.UseVisualStyleBackColor = true;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 96);
+            this.Controls.Add(this.LoginBtn);
+            this.Controls.Add(this.PasswordTB);
+            this.Controls.Add(this.UsernameTB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UsernameTB;
+        private System.Windows.Forms.TextBox PasswordTB;
+        private System.Windows.Forms.Button LoginBtn;
+    }
+}
---
+++ 2. pololetí/Domácí úkoly/Flajžík/TodolistUkol2/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using TodoList.Data;
+using System.Collections.Generic;
+
+namespace TodoList
+{
+    public partial class MainForm : Form
+    {
+        private BindingList<TodoItem> _data = new BindingList<TodoItem>();
+
+        // List<TodoItem> _data = new List<TodoItem>() => nezobrazuje eventy v listboxu
+        public MainForm()
+        {
+            InitializeComponent();
+            //zobrazí události z listu v listboxu
+           
+            ListLB.ValueMember = nameof(TodoItem.DisplayName);
+            
+            ListLB.DataSource = _data;
+
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            var happen = new TodoItem();
+            happen.Name = TextTB.Text;
+            happen.Date = DateDTP.Value;
+
+            _data.Add(happen);
+
+            MessageBox.Show("Přidal jste novou událost: " + happen.Name + "\n číslo:  " + happen.Id.ToString());
+
+
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            var item = ListLB.SelectedItem as TodoItem;
+            if(item != null)
+            {
+                if (MessageBox.Show
+            ("Opravdu chcete smazat událost?", "Odstranění",
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question)
+            == DialogResult.Yes)
+                {
+                    _data.Remove(item);
+                }
+
+
+
+            }
+
+
+        }
+    }
+}
