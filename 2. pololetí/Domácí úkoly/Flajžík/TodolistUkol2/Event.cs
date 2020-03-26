48de62b3a5b3ba09cd0e022aff8e46b2c4debc0f
JanFlajz <62598168+JanFlajz@users.noreply.github.com>
2020-03-26 11:51:25 +0100

    DÚ Jan Flajžík (#5)
    * Add files via upload
    
    Todo list ukol
    
    * TodolistUkol2
    
    * Delete TodolistUkol2
    
    * h
    
    * Add files via upload
    
    * Delete TodoList.sln
    
    * Delete TodoList.csproj.user
    
    * Delete TodoList.csproj
    
    * Delete Login.cs
    
    * Delete Program.cs
    
    * Delete MainForm.resx
    
    * Delete MainForm.cs
    
    * Delete MainForm.Designer.cs
    
    * Delete Login.resx
    
    * Delete Event.cs
    
    * Delete Login.Designer.cs
    
    * Delete App.config
    
    * Delete .gitignore
    
    * Add files via upload
    
    * Delete .gitattributes

---
+++ 2. pololetí/Domácí úkoly/Flajžík/TodolistUkol2/Event.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoList
+{
+    class Event
+    {
+        public Guid Id { get; set; } = new Guid();
+        public  string Name { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Displayname => $"{Date.ToShortDateString()} - {Name}";
+
+
+    }
+}
